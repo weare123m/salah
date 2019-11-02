592fba71b696b01132d48cb0e659b3ce7ac288f9
weare123m <54211243+weare123m@users.noreply.github.com>
2019-11-02 21:30:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,54 +90,44 @@ client.on('message', message => {
 }
 });
 client.on('message', message => {
-  var args = message.content.split(" ").slice(1);    
-  if(message.content.startsWith(prefix + 'id')) {
-  var year = message.author.createdAt.getFullYear()
-  var month = message.author.createdAt.getMonth()
-  var day = message.author.createdAt.getDate()
-  var men = message.mentions.users.first();  
-  let args = message.content.split(' ').slice(1).join(' ');
-  if (args == '') {
-  var z = message.author;
-  }else {
-  var z = message.mentions.users.first();
-  }
-  
-  let d = z.createdAt;          
-  let n = d.toLocaleString();   
-  let x;                       
-  let y;                        
-  
-  if (z.presence.game !== null) {
-  y = `${z.presence.game.name}`;
-  } else {
-  y = "No Playing...";
-  }
-  if (z.bot) {
-  var w = 'BOT';
-  }else {
-  var w = 'MEMBER';
-  }
-  let embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setTitle(`**INFO** ${z.username}`)
-  .addField('`Your Name`',`**<@` + `${z.id}` + `>**`, true)
-  .addField('`ID`', "**"+ `${z.id}` +"**",true)
-  .addField('`Status`','**'+y+'**' , true)
-  .addField('`Acount Type`',"**"+ w + "**",true)    
-  .addField('`Your Tag`',"**#" +  `${z.discriminator}**`,true)
-  .addField('`Your account created in`' ,year + "-"+ month +"-"+ day)    
-  .addField("`Entered the server in`", message.member.joinedAt.toLocaleString())    
-  .addField("`Last Message`", message.author.lastMessage)            
-  
-  .setThumbnail(`${z.avatarURL}`)
-  .setFooter(message.author.username, message.author.avatarURL)
-  
-  message.channel.send({embed});
-      if (!message) return message.reply('**ضع المينشان بشكل صحيح  ? **')
-  
-  }
-  });
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'clear')) {
+if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let request = `Requested By ${message.author.username}`;
+message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react(':negative_squared_cross_mark:'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+var msg;
+        msg = parseInt();
+
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "`` Chat Deleted ``",
+        color: 0x06DF00,
+        footer: {
+
+        }
+      }}).then(msg => {msg.delete(3000)});
+
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
 
 client.on('ready', () => {
     console.log(`----------------`);
