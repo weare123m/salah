cae59fd1807580f3ca2bbad7970c0bf33cb98675
weare123m <54211243+weare123m@users.noreply.github.com>
2019-11-02 19:41:27 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "*";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "send")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+client.on('ready', () => {
+    console.log(`----------------`);
+       console.log(`Desert Bot- Script By : le-titiz `);
+         console.log(`----------------`);
+       console.log(`ON ${client.guilds.size} Servers '     Script By : </> ' `);
+     console.log(`----------------`);
+   console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setGame(`system bot`,"http://twitch.tv/krunker")
+ client.user.setStatus("dnd")
+ });
+ 
+ 
+ client.login(process.env.BOT_TOKEN);
