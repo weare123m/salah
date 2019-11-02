beff375c6befbece2b634c7dc872e2a2bf0cdc78
weare123m <54211243+weare123m@users.noreply.github.com>
2019-11-02 21:10:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,62 @@ message.channel.send(embed)
 
 
 });
+client.on('message', function(msg) {
+    if(msg.content.startsWith (prefix  + 'server')) {
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`**Showing Details Of** ${msg.guild.name}`)
+      .addField('`Server Region`',`[${msg.guild.region}]`,true)
+      .addField('`Roles Count`',`[${msg.guild.roles.size}]`,true)
+      .addField('`Members Count`',`[${msg.guild.memberCount}]`,true)
+      .addField('`Online Members`',`[${msg.guild.members.filter(m=>m.presence.status == 'online').size}]`,true)
+      .addField('`Text Channels`',`[${msg.guild.channels.filter(m => m.type === 'text').size}]`,true)
+      .addField('`Voice Channels`',`[${msg.guild.channels.filter(m => m.type === 'voice').size}]`,true)
+      .addField('`Server Owner`',`**${msg.guild.owner}**`,true)
+      .addField('`Server Id`',`**${msg.guild.id}**`,true)
+      .addField('`Server was created in`',msg.guild.createdAt.toLocaleString())
+      msg.channel.send({embed:embed})
+    }
+});
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Salah Community ©`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+			      .addField('``My Prefix``' , `[ ! ]` , true)
+			      .addField('``My Language``' , `[ Java Script ]` , true)
+			      .setFooter('By | Salah')
+    })
+}
+});
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='G.count')
+	 
+      message.reply(`**${message.guild.memberCount}**`);
+    });
+    client.on('message', message => {
+   if(message.content.startsWith(prefix + "invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
+});
+ client
 client.on('ready', () => {
     console.log(`----------------`);
        console.log(`Desert Bot- Script By : le-titiz `);
